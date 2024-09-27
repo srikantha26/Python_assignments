23e10165a6350273fe6c2cde04aa9dbb52d612cd
Srikanth Avutapalli <srikanthavutapalli@Srikanths-MacBook-Pro.local>
2024-09-27 14:36:16 +1000

    added latest

---
+++ lesson_Mathematical_operations.py
@@ -0,0 +1,15 @@
+print("my age:"+str(12))
+print(123+123)
+print(123-123)
+print(6/3)
+print(6//3)
+print(2**2)
+
+#PEMDAS LR
+#parantheses,exponents,multiplication/division,Addition/Subtraction
+# ()
+# **
+# * OR /
+# + OR -
+
+print(3 * 3 + 3 / 3 -3)
--- main.py
+++ main.py
@@ -1,2 +1,5 @@
 print("welcome to Python Project")
 print("second test statement")
+
+# to comment multiple lines in python
+# use command + /
